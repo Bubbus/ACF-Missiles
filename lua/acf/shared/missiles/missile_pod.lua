13d043570c7551618419c5ee6c3b09c166b0f29d
Bubbus <splambob@googlemail.com>
2015-02-05 23:34:35 +0000

    Added pod rocket definitions - not balanced.

---
+++ lua/acf/shared/missiles/missile_pod.lua
@@ -0,0 +1,103 @@
+
+--define the class
+ACF_defineGunClass("FFAR", {
+    type            = "missile",
+	spread          = 1,
+	name            = "Folding-Fin Aerial Rockets",
+	desc            = "Small rockets which fit in tubes or pods.  Rapid-firing and versatile.",
+	muzzleflash     = "40mm_muzzleflash_noscale",
+	rofmod          = 0.5,
+	sound           = "acf_extra/airfx/rocket_fire2.wav",
+	soundDistance   = " ",
+	soundNormal     = " ",
+    effect          = "Rocket Motor",
+    
+    ammoBlacklist   = {"AP", "APHE", "FL"} -- Including FL would mean changing the way round classes work.
+} )
+
+
+
+
+ACF_defineGun("40mmFFAR", { --id
+
+	name		= "40mm Pod Rocket",
+	desc		= "A tiny, unguided rocket.  Useful for anti-infantry, smoke and suppression.  Folding fins allow the rocket to be stored in pods, which defend them from damage.",
+	model		= "models/missiles/launcher7_40mm.mdl",
+	caliber		= 4,
+	gunclass	= "FFAR",
+    rack        = "40mm7xPOD",  -- Which rack to spawn this missile on?
+	weight		= 6,
+    length	    = 32,
+    rofmod		= 1.25,
+	year		= 1960,
+	round		= 
+	{
+		model		= "models/missiles/ffar_40mm.mdl",
+		rackmdl		= "models/missiles/ffar_40mm_closed.mdl",
+		maxlength	= 32,
+		casing		= 0.2,	// thickness of missile casing, cm
+		propweight	= 1,	// motor mass - motor casing
+		thrust		= 300*39.37,	// average thrust - kg*in/s^2
+		burnrate	= 450,	// cm^3/s at average chamber pressure
+        starterpct	= 0.15,         -- percentage of the propellant consumed in the starter motor.
+        minspeed	= 3000,			-- minimum speed beyond which the fins work at 100% efficiency
+        dragcoef	= 0.004,		-- drag coefficient of the missile
+		finmul		= 0.01			-- fin multiplier (mostly used for unpropelled guidance)
+	},
+    
+    ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
+    guidance    = {"Dumb"},
+    fuses       = ACF_GetAllFuseNamesExcept( {"Radio"} ),
+    
+    racks       = {["40mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
+    
+    seekcone    = 35,   -- getting inside this cone will get you locked.  Divided by 2 ('seekcone = 40' means 80 degrees total.)
+    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2. 
+    
+    agility     = 1     -- multiplier for missile turn-rate. 
+} )
+
+
+
+
+ACF_defineGun("70mmFFAR", { --id
+
+	name		= "70mm Pod Rocket",
+	desc		= "A small, unguided rocket.  Useful against light vehicles and infantry.  Folding fins allow the rocket to be stored in pods, which defend them from damage.",
+	model		= "models/missiles/launcher7_70mm.mdl",
+	caliber		= 7,
+	gunclass	= "FFAR",
+    rack        = "70mm7xPOD",  -- Which rack to spawn this missile on? 
+    weight		= 12,
+    length	    = 26*1.75,
+	year		= 1960,
+	rofmod		= 0.75,
+	roundclass	= "Rocket",
+	round		= 
+	{
+		model		= "models/missiles/ffar_70mm.mdl",
+		rackmdl		= "models/missiles/ffar_70mm_closed.mdl",
+		maxlength	= 26*1.75,
+		casing		= 0.2,	// thickness of missile casing, cm
+		propweight	= 1.75,	// motor mass - motor casing
+		thrust		= 400*39.37,	// average thrust - kg*in/s^2
+		burnrate	= 450,	// cm^3/s at average chamber pressure
+		starterpct	= 0.15,
+        minspeed	= 3000,			-- minimum speed beyond which the fins work at 100% efficiency
+        dragcoef	= 0.004,		-- drag coefficient of the missile
+		finmul		= 0.01			-- fin multiplier (mostly used for unpropelled guidance)
+	},
+	
+    ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
+    guidance    = {"Dumb"},
+    fuses       = ACF_GetAllFuseNamesExcept( {"Radio"} ),
+    
+    racks       = {["70mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
+    
+    seekcone    = 35,   -- getting inside this cone will get you locked.  Divided by 2 ('seekcone = 40' means 80 degrees total.)
+    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2. 
+    
+    agility     = 1     -- multiplier for missile turn-rate.
+} )
+
+
