72c2acfe218461bb03bd3f9ca1ad70fe0212e59c
ColBeckett <turboplus2@gmail.com>
2015-02-24 23:02:33 +0100

    - Another Bomb ballance
    - added dummy 1000kg bomb (use 500kg bomb model)
    - speed up bomb reloading

--- lua/acf/shared/missiles/missile_bomb.lua
+++ lua/acf/shared/missiles/missile_bomb.lua
@@ -6,7 +6,7 @@ ACF_defineGunClass("BOMB", {
 	name            = "Bomb",
 	desc            = "Free-falling bombs.  These are slower than missiles, but are more powerful and more manouverable when guided.",
 	muzzleflash     = "40mm_muzzleflash_noscale",
-	rofmod          = 1,
+	rofmod          = 0.1,
 	sound           = "acf_extra/tankfx/clunk.wav",
 	soundDistance   = " ",
 	soundNormal     = " ",
@@ -37,7 +37,7 @@ ACF_defineGun("50kgBOMB", { --id
 	round = {
 		model		= "models/bombs/fab50.mdl",
 		rackmdl		= "models/bombs/fab50.mdl",
-		maxlength	= 60,
+		maxlength	= 50,
 		casing		= 0.5,	        -- thickness of missile casing, cm
 		propweight	= 0,	        -- motor mass - motor casing
 		thrust		= 1,	    -- average thrust - kg*in/s^2
@@ -77,7 +77,7 @@ ACF_defineGun("100kgBOMB", { --id
 	round = {
 		model		= "models/bombs/fab100.mdl",
 		rackmdl		= "models/bombs/fab100.mdl",
-		maxlength	= 70,
+		maxlength	= 75,
 		casing		= 0.5,	        -- thickness of missile casing, cm
 		propweight	= 0,	        -- motor mass - motor casing
 		thrust		= 1,	    -- average thrust - kg*in/s^2
@@ -107,14 +107,14 @@ ACF_defineGun("250kgBOMB", { --id
 	gunclass = "BOMB",
     rack = "1xRK",  -- Which rack to spawn this missile on?
 	length = 25,
-	caliber = 25.0,
+	caliber = 12.5,
 	weight = 250,    -- Don't scale down the weight though!
 	year = 1941,
     modeldiameter = 16.3 * 1.9, -- in cm
 	round = {
 		model		= "models/bombs/fab250.mdl",
 		rackmdl		= "models/bombs/fab250.mdl",
-		maxlength	= 110,
+		maxlength	= 125,
 		casing		= 0.5,	        -- thickness of missile casing, cm
 		propweight	= 0,	        -- motor mass - motor casing
 		thrust		= 1,	    -- average thrust - kg*in/s^2
@@ -145,14 +145,14 @@ ACF_defineGun("500kgBOMB", { --id
 	gunclass = "BOMB",
     rack = "1xRK",  -- Which rack to spawn this missile on?
 	length = 50,
-	caliber = 50.0,
+	caliber = 30.0,
 	weight = 500,    -- Don't scale down the weight though!
 	year = 1943,
     modeldiameter = 16.3 * 1.9, -- in cm
 	round = {
 		model		= "models/bombs/fab500.mdl",
 		rackmdl		= "models/bombs/fab500.mdl",
-		maxlength	= 175,
+		maxlength	= 250,
 		casing		= 0.5,	        -- thickness of missile casing, cm
 		propweight	= 0,	        -- motor mass - motor casing
 		thrust		= 1,	    -- average thrust - kg*in/s^2
@@ -174,6 +174,44 @@ ACF_defineGun("500kgBOMB", { --id
     
     agility     = 1     -- multiplier for missile turn-rate.
 } )
+
+ACF_defineGun("1000kgBOMB", { --id
+	name = "1000kg Free Falling Bomb",
+	desc = "2000lb bomb, biggest projectile in acf - keep it for some slow big target.",
+	model = "models/bombs/fab500.mdl",
+	gunclass = "BOMB",
+    rack = "1xRK",  -- Which rack to spawn this missile on?
+	length = 75,
+	caliber = 50.0,
+	weight = 1000,    -- Don't scale down the weight though!
+	year = 1945,
+    modeldiameter = 16.3 * 1.9, -- in cm
+	round = {
+		model		= "models/bombs/fab500.mdl",
+		rackmdl		= "models/bombs/fab500.mdl",
+		maxlength	= 500,
+		casing		= 0.5,	        -- thickness of missile casing, cm
+		propweight	= 0,	        -- motor mass - motor casing
+		thrust		= 1,	    -- average thrust - kg*in/s^2
+		burnrate	= 1,	        -- cm^3/s at average chamber pressure
+		starterpct	= 0.005,          -- percentage of the propellant consumed in the starter motor.
+		minspeed	= 1,			-- minimum speed beyond which the fins work at 100% efficiency
+		dragcoef	= 0.002,		-- drag coefficient of the missile
+		finmul		= 0.002			-- fin multiplier (mostly used for unpropelled guidance)
+	},
+   
+    ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
+    guidance    = {"Dumb"},
+    fuses       = ACF_GetAllFuseNamesExcept( {"Radio"} ),
+ 
+	racks       = {["1xRK"] = true},   -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
+ 
+    seekcone    = 40,   -- getting inside this cone will get you locked.  Divided by 2 ('seekcone = 40' means 80 degrees total.)
+    viewcone    = 60,   -- getting outside this cone will break the lock.  Divided by 2. 
+    
+    agility     = 1     -- multiplier for missile turn-rate.
+} )
+
 
 
 /*
