af1c9bb1e95140a4a9a2e6db89103624e094054f
Bubbus <splambob@googlemail.com>
2015-02-27 21:32:25 +0000

    Missed a comma

--- lua/acf/shared/missiles/missile_bomb.lua
+++ lua/acf/shared/missiles/missile_bomb.lua
@@ -13,7 +13,7 @@ ACF_defineGunClass("BOMB", {
     
     reloadmul       = 8,
     
-    ammoBlacklist   = {"AP", "HEAT" "FL"} -- Including FL would mean changing the way round classes work.
+    ammoBlacklist   = {"AP", "HEAT", "FL"} -- Including FL would mean changing the way round classes work.
 } )
 
 
