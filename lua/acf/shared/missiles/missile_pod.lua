7cf3b857acbfcce378d016874f3a566524648e90
Cre8or <philipp.cre@gmail.com>
2015-11-07 12:57:59 +0100

    - Edited UARs and FFARs to work with the new guidance system - Changed flare launcher sound

--- lua/acf/shared/missiles/missile_pod.lua
+++ lua/acf/shared/missiles/missile_pod.lua
@@ -11,7 +11,7 @@ ACF_defineGunClass("FFAR", {
 	soundDistance   = " ",
 	soundNormal     = " ",
     effect          = "Rocket Motor",
-    
+
     ammoBlacklist   = {"AP", "APHE", "FL"} -- Including FL would mean changing the way round classes work.
 } )
 
@@ -30,7 +30,90 @@ ACF_defineGun("40mmFFAR", { --id
     length	    = 2, -- Length affects inertia calculations
     rofmod		= 0.7,
 	year		= 1960,
-	round		= 
+	round		=
+	{
+		model		= "models/missiles/ffar_40mm.mdl",
+		rackmdl		= "models/missiles/ffar_40mm_closed.mdl",
+		maxlength	= 25,
+		casing		= 0.2,			-- thickness of missile casing, cm
+		armour		= 4,			-- effective armour thickness of casing, in mm
+		propweight	= 0.2,			-- motor mass - motor casing
+		thrust		= 30000,		-- average thrust - kg*in/s^2
+		burnrate	= 150,			-- cm^3/s at average chamber pressure
+        starterpct	= 0.15,         -- percentage of the propellant consumed in the starter motor.
+        minspeed	= 6000,			-- minimum speed beyond which the fins work at 100% efficiency
+        dragcoef	= 0.003,		-- drag coefficient while falling
+        dragcoefflight  = 0.03,                 -- drag coefficient during flight
+		finmul		= 0.005,		-- fin multiplier (mostly used for unpropelled guidance)
+		penmul		= math.sqrt(4)
+	},
+
+    ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
+    guidance    = {"Dumb"},
+    fuses       = {"Contact", "Timed"},
+
+    racks       = {["40mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
+
+    armdelay    = 0.3     -- minimum fuse arming delay
+} )
+
+
+
+
+ACF_defineGun("70mmFFAR", { --id
+
+	name		= "70mm Pod Rocket",
+	desc		= "A small, unguided rocket.  Useful against light vehicles and infantry.  Folding fins allow the rocket to be stored in pods, which defend them from damage.",
+	model		= "models/missiles/launcher7_70mm.mdl",
+	caliber		= 7,
+	gunclass	= "FFAR",
+    rack        = "70mm7xPOD",  -- Which rack to spawn this missile on?
+    weight		= 12,
+    length	    = 15,
+	year		= 1960,
+	rofmod		= 0.6,
+	roundclass	= "Rocket",
+	round		=
+	{
+		model		= "models/missiles/ffar_70mm.mdl",
+		rackmdl		= "models/missiles/ffar_70mm_closed.mdl",
+		maxlength	= 25,
+		casing		= 0.2,			-- thickness of missile casing, cm
+		armour		= 6,			-- effective armour thickness of casing, in mm
+		propweight	= 0.7,			-- motor mass - motor casing
+		thrust		= 50000,		-- average thrust - kg*in/s^2
+		burnrate	= 400,			-- cm^3/s at average chamber pressure
+		starterpct	= 0.15,
+        minspeed	= 6000,			-- minimum speed beyond which the fins work at 100% efficiency
+        dragcoef	= 0.004,		-- drag coefficient while falling
+        dragcoefflight  = 0.03,                 -- drag coefficient during flight
+	finmul		= 0.008,			-- fin multiplier (mostly used for unpropelled guidance)
+        penmul      = math.sqrt(6)  	-- HEAT velocity multiplier. Squared relation to penetration (math.sqrt(2) means 2x pen)
+	},
+
+    ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
+    guidance    = {"Dumb"},
+    fuses       = {"Contact", "Timed"},
+
+    racks       = {["70mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
+
+    armdelay    = 0.3     -- minimum fuse arming delay
+} )
+
+/*
+ACF_defineGun("40mmFFAR", { --id
+
+	name		= "40mm Pod Rocket",
+	desc		= "A tiny, unguided rocket.  Useful for anti-infantry, smoke and suppression.  Folding fins allow the rocket to be stored in pods, which defend them from damage.",
+	model		= "models/missiles/launcher7_40mm.mdl",
+	caliber		= 4,
+	gunclass	= "FFAR",
+    rack        = "40mm7xPOD",  -- Which rack to spawn this missile on?
+	weight		= 6,
+    length	    = 2, -- Length affects inertia calculations
+    rofmod		= 0.7,
+	year		= 1960,
+	round		=
 	{
 		model		= "models/missiles/ffar_40mm.mdl",
 		rackmdl		= "models/missiles/ffar_40mm_closed.mdl",
@@ -46,17 +129,17 @@ ACF_defineGun("40mmFFAR", { --id
 		finmul		= 0.003,		-- fin multiplier (mostly used for unpropelled guidance)
 		penmul		= math.sqrt(4)
 	},
-    
+
     ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
     guidance    = {"Dumb"},
     fuses       = {"Contact", "Timed"},
-    
+
     racks       = {["40mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
-    
+
     seekcone    = 35,   -- getting inside this cone will get you locked.  Divided by 2 ('seekcone = 40' means 80 degrees total.)
-    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2. 
-    
-    agility     = 1,     -- multiplier for missile turn-rate. 
+    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2.
+
+    agility     = 1,     -- multiplier for missile turn-rate.
     armdelay    = 0.3     -- minimum fuse arming delay
 } )
 
@@ -70,13 +153,13 @@ ACF_defineGun("70mmFFAR", { --id
 	model		= "models/missiles/launcher7_70mm.mdl",
 	caliber		= 7,
 	gunclass	= "FFAR",
-    rack        = "70mm7xPOD",  -- Which rack to spawn this missile on? 
+    rack        = "70mm7xPOD",  -- Which rack to spawn this missile on?
     weight		= 12,
     length	    = 15,
 	year		= 1960,
 	rofmod		= 0.6,
 	roundclass	= "Rocket",
-	round		= 
+	round		=
 	{
 		model		= "models/missiles/ffar_70mm.mdl",
 		rackmdl		= "models/missiles/ffar_70mm_closed.mdl",
@@ -92,18 +175,17 @@ ACF_defineGun("70mmFFAR", { --id
 		finmul		= 0.003,			-- fin multiplier (mostly used for unpropelled guidance)
         penmul      = math.sqrt(6)  	-- HEAT velocity multiplier. Squared relation to penetration (math.sqrt(2) means 2x pen)
 	},
-	
+
     ent         = "acf_missile_to_rack", -- A workaround ent which spawns an appropriate rack for the missile.
     guidance    = {"Dumb"},
     fuses       = {"Contact", "Timed"},
-    
+
     racks       = {["70mm7xPOD"] = true},    -- a whitelist for racks that this missile can load into.  can also be a 'function(bulletData, rackEntity) return boolean end'
-    
+
     seekcone    = 35,   -- getting inside this cone will get you locked.  Divided by 2 ('seekcone = 40' means 80 degrees total.)
-    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2. 
-    
+    viewcone    = 55,   -- getting outside this cone will break the lock.  Divided by 2.
+
     agility     = 1,     -- multiplier for missile turn-rate.
     armdelay    = 0.3     -- minimum fuse arming delay
 } )
-
-
+*/
