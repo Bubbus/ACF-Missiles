66977fc9e969a3df9c4daa78a8dd340d11f8632a
ColBeckett <turboplus2@gmail.com>
2015-12-17 15:48:57 +0100

    fixed radar sizes again
    added gbu16 models with WIP textures
    and new mid radar size (75% of big instead of 60%)

--- lua/acf/shared/radars/radar_directional.lua
+++ lua/acf/shared/radars/radar_directional.lua
@@ -11,7 +11,7 @@ ACF_DefineRadar("SmallDIR", {
 	name 		= "Small Directional Radar",
 	ent			= "acf_missileradar",
 	desc 		= "A lightweight directional radar with a smaller view cone.",
-	model		= "models/radar/radar_mid.mdl",
+	model		= "models/radar/radar_sml.mdl",
 	class 		= "DIR",
 	weight 		= 300,
 	viewcone 	= 30 -- half of the total cone.  'viewcone = 30' means 60 degs total viewcone.
@@ -22,7 +22,7 @@ ACF_DefineRadar("MediumDIR", {
 	name 		= "Medium Directional Radar",
 	ent			= "acf_missileradar",
 	desc 		= "A directional radar with a regular view cone.",
-	model		= "models/radar/radar_sml.mdl", -- medium one is for now scalled big one - will be changed
+	model		= "models/radar/radar_mid.mdl", -- medium one is for now scalled big one - will be changed
 	class 		= "DIR",
 	weight 		= 600,
 	viewcone 	= 45 -- half of the total cone.  'viewcone = 30' means 60 degs total viewcone.
