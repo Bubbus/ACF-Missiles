0f1dfb98c32e901cd8f647897947762c9924f563
Tengz <redcraftrus@gmail.com>
2016-08-14 11:09:02 +0300

    first l33trs update deleted gitrc, update checker and wiki

--- lua/autorun/acf_missiles_include.lua
+++ lua/autorun/acf_missiles_include.lua
@@ -9,22 +9,12 @@ include("acf/shared/acf_missileloader.lua")
 AddCSLuaFile("acf/shared/acfm_globals.lua")
 include("acf/shared/acfm_globals.lua")
 
-AddCSLuaFile("autorun/client/cl_acfm_versioncheck.lua")
 AddCSLuaFile("autorun/client/cl_acfm_menuinject.lua")
 AddCSLuaFile("autorun/client/cl_acfm_effectsoverride.lua")
 AddCSLuaFile("autorun/printbyname.lua")
 AddCSLuaFile("acf/client/cl_acfmenu_missileui.lua")
 
-if SERVER then
-
-  include("gitrc.lua")
-
-end
-
 AddCSLuaFile("includes/modules/markdown.lua")
-AddCSLuaFile("acf/client/cl_missilewiki.lua")
-AddCSLuaFile("autorun/client/acfm_wiki.lua")
-
 
 AddCSLuaFile("acf/shared/sh_acfm_getters.lua")
 AddCSLuaFile("autorun/sh_acfm_roundinject.lua")
