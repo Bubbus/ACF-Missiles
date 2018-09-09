11b2027b71496fb273a6ccb938801cb73db40018
PolySteam <38295124+PolySteam@users.noreply.github.com>
2018-09-09 04:16:28 +0300

    Update shared.lua

--- lua/entities/acf_opticalcomputer/shared.lua
+++ lua/entities/acf_opticalcomputer/shared.lua
@@ -1,2 +1,6 @@
-ENT.Type 		= "anim"
-ENT.Base 		= "base_anim"
+ENT.Type = "anim"
+ENT.PrintName = "GLATGM Optical Computer"
+ENT.Author = "Polymorphic Turtle"
+ENT.Category = "ACF"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
