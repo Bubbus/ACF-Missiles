9c680a79fe06970a52e12dbcb5a5b2ecb198e660
PolySteam <38295124+PolySteam@users.noreply.github.com>
2018-09-10 06:34:47 +0300

    GLATGM balance changes

--- lua/entities/acf_glatgm/init.lua
+++ lua/entities/acf_glatgm/init.lua
@@ -33,6 +33,23 @@ function ENT:Initialize()
 		end
 	end
 
+	self.velocity = 5000 		--self.velocity of the missile per second
+	self.secondsOffset = 0.5	--seconds of forward flight to aim towards, to affect the beam-riding simulation
+	
+	
+	
+	
+	self.Sub = self.BulletData.Caliber<10 
+	if self.Sub then 
+	self.velocity = 2500
+	self.secondsOffset = 0.25
+	self.SpiralAm = (10-self.BulletData.Caliber)*0.5 -- amount of artifical spiraling for <100 shells, caliber in acf is in cm
+	end
+	
+	
+	
+	
+	self.offsetLength = self.velocity * self.secondsOffset	--how far off the forward offset is for the targeting position
 end
 
 
@@ -59,26 +76,33 @@ function ENT:Think()
 			end
 			local TimeNew = CurTime()
 			
+
+			
 			local d = Vector(0,0,0)
 			local dir = AngleRand()*0.01
-			local Dist = 100/10000
+			local Dist = 0.01--100/10000
 			if IsValid(self.Guidance) and self.Guidance:GetPos():Distance(self:GetPos())<self.Distance then
 				local di = self.Guidance:WorldToLocalAngles((self:GetPos() - self.Guidance:GetPos()):Angle())
 				if di.p<15 and di.p>-15 and di.y<15 and di.y>-15 then
 					local glpos = self.Guidance:GetPos()+self.Guidance:GetForward()
-					if !self.Optic then
+					if not self.Optic then
 						glpos = self.Guidance:GetAttachment(1).Pos+self.Guidance:GetForward()*20
 					end
 
-					local tr = util.QuickTrace( glpos, self.Guidance:GetForward()*99999, {self.Guidance,self,self.Entity}) 
+					local tr = util.QuickTrace( glpos, self.Guidance:GetForward()*(self.Guidance:GetPos():Distance(self:GetPos())+self.offsetLength), {self.Guidance,self,self.Entity})
+	--				local tr = util.QuickTrace( glpos, self.Guidance:GetForward()*99999, {self.Guidance,self,self.Entity}) --outdated
 					d = ( tr.HitPos - self:GetPos())
-					dir = self:WorldToLocalAngles(d:Angle())*0.01
+					dir = self:WorldToLocalAngles(d:Angle())*0.02 --0.01 controls agility but is not scaled to timestep; bug poly
 					 Dist = self.Guidance:GetPos():Distance(self:GetPos())/39.37/10000
 				end
 			end
+			local Spiral = d:Length()/39370 or 0.5
+			if self.Sub then
+				Spiral = self.SpiralAm --Spaghett
+			end
 			local Inacc = math.random(-1,1)*Dist
 			self:SetAngles(self:LocalToWorldAngles(dir+Angle(Inacc,-Inacc,5)))
-			self:SetPos(self:LocalToWorld(Vector((4000)*(TimeNew - self.Time),d:Length()/39370 or 0.5,0)))
+			self:SetPos(self:LocalToWorld(Vector((self.velocity)*(TimeNew - self.Time),Spiral,0)))
 			local tr = util.QuickTrace( self:GetPos(), self:GetForward()*300, {self,self.Entity}) 
 			
 			self.Time = TimeNew
@@ -90,7 +114,6 @@ function ENT:Think()
 		return true
 	end
 end
-
 
 
 
@@ -163,7 +186,7 @@ function ENT:Detonate()
 	
 	
 
-	
+	self.FakeCrate:Remove()
 	self:Remove()
 
 
